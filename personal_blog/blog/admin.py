42c25059d54918abbed1223f0023d49e5c81dc68
Adam Szustak <madasz@onet.eu>
2020-05-12 11:58:32 +0200

    admin site shows url

--- personal_blog/blog/admin.py
+++ personal_blog/blog/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Post, Category
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("field", "title", "created_on", "status")
+    list_display = ("field", "title", "created_on", "status", "show_url")
     list_filter = ("status",)
     search_fields = [
         "title",
         "tags",
     ]
     prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ("show_url",)
+
+    def show_url(self, instance):
+        url = reverse('post_detail', kwargs={'slug': instance.slug})
+        response = format_html(f"<a href={url}>{url}</a>")
+        return response
+    
+    show_url.short_description = "Post Url"
 
 
 class CategoryAdmin(admin.ModelAdmin):
